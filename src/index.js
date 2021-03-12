a1c5625460e947c46fcacccba3a7d65b0bab17ae
Douglas Menezes <53621343+menezedouglas@users.noreply.github.com>
2021-03-11 22:34:03 -0300

    Correções no deploy

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import { component as fullWidthComponent } from '@/modules/full'
+import { component as full } from '@/modules/full'
+import example from './App'
 
-export function install (Vue) {
-  if (install.installed) return
-  install.installed = true
-  Vue.component('calendar-full-width', fullWidthComponent)
+export default {
+  full,
+  example
 }
-
-const plugin = {
-  install
-}
-
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue
-}
-
-if (GlobalVue) {
-  GlobalVue.use(plugin)
-}
-
-export default fullWidthComponent
